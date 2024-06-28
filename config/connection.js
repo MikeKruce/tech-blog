99c55ce306dc60065e3a66fd40f57c0361da8eb4
Mikek1977 <mkrucejr@gmail.com>
2024-06-27 22:03:25 -0400

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT, // 'postgres' for PostgreSQL
-    port: 5432, // Default PostgreSQL port
+    dialect: 'postgres', 
+    port: 5432, 
   }
 );
 
